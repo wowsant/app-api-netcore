48476cff101e104256e1162215462b9b346a85a1
Sant' Clair da Silva <sant.clair@outlook.com>
2022-12-25 22:15:58 -0300

    Include dependency mapper

---
+++ app.CrossCutting/Assemblies/AssemblyUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace app.CrossCutting.Assemblies
+{
+    public static class AssemblyUtil
+    {
+        public static IEnumerable<Assembly> GetCurrentAssemblies()
+        {
+            return new Assembly[]
+            {
+                Assembly.Load("app.Api"),
+                Assembly.Load("app.Application"),
+                Assembly.Load("app.Domain"),
+                Assembly.Load("app.Infrastructure"),
+                Assembly.Load("app.CrossCutting"),
+                Assembly.Load("app.Util")
+            };
+        }
+    }
+}
---
+++ app.CrossCutting/IoC/DependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace app.CrossCutting.IoC
+{
+    public static class DependencyResolver
+    {
+        public static void AddDependencyResolver(this IServiceCollection services)
+        {
+            RegisterApplications(services);
+            //RegisterRepositories(services);
+            RegisterAdapters(services);
+            RegisterApis(services);
+            RegisterPublishers(services);
+        }
+        private static void RegisterApplications(IServiceCollection services)
+        {
+            //services.AddSingleton<ITitleApplication, TitleApplication>();
+        }
+        private static void RegisterAdapters(IServiceCollection services)
+        {
+            //services.AddTransient(provider => new SqlConnection(Configurations.PagerDbConnectionString));
+            //services.AddTransient<ITitlePort, TitleAdapter>();
+        }
+
+        /*
+
+        private static void RegisterRepositories(IServiceCollection services)
+        {
+
+        }
+ 
+        */
+        private static void RegisterApis(IServiceCollection services)
+        {
+            //services.AddSingleton<IEmail, Email>();
+            //services.AddSingleton<IEmailCampaign, EmailCampaign>();
+            //services.AddSingleton<IEmailAttribute, EmailAttribute>();
+
+        }
+        private static void RegisterPublishers(IServiceCollection services)
+        {
+            //services.AddSingleton<ITitlePublisher, TitlePublisher>();
+        }
+    }
+}
